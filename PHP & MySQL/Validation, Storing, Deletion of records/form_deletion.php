db7ab95250d1be08e069ae391587c117218e1e3f
Alarixc <127346578+Alarixc@users.noreply.github.com>
2023-09-18 20:11:41 +0530

    Create form_deletion.php

---
+++ PHP & MySQL/Validation, Storing, Deletion of records/form_deletion.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Deletion Form</title>
+</head>
+<body>
+    <p>Delete the previous entries using registration number</p>
+    <form method="post" action="test3.php">
+        <label for="regno">Registration no:</label>
+        <input type="text" name="regno">
+        <br><br>
+
+        <button type="submit">
+            Delete
+        </button>
+    </form>
+</body>
+</html>
